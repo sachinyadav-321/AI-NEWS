0078c3b339083613a0f4882f11d671c65faa734f
sachinyadav-321 <sachinyad9958@gmail.com>
2024-05-24 09:43:22 +0530

    Adding API-KEY

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "335efd56b3ff4dcbbda9274ed0e0b0cf";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
